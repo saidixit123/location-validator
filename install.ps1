29dc5c4c799b8c89f70f28c0acb82918e66d3b95
Sai Dixit Naidu <116658402+saidixit123@users.noreply.github.com>
2025-06-06 14:17:59 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,13 @@
+# Clone the repo
 git clone https://github.com/saidixit123/location-validator.git
 cd location-validator/scraper
+
+# Set up virtual environment
 python -m venv venv
 .\venv\Scripts\activate
+
+# Install required packages
 pip install selenium gspread google-auth
+
+# Run the scraper
 python main.py
